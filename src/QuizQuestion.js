2ebec672b6aed9d6e5c11a04506e4d3026f52415
vinod2159 <vinod.yadav@mindtree.com>
2022-04-10 15:20:42 +0530

    Fixed#2

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react'
 
-
 class QuizQuestion extends Component {
-    constructor(props) {
-      super(props)
-    }
-    render() {
-      return (
-        <main>
+  render() {
+    return (
+      <main>
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
           <ul>
-             <li>{this.props.quiz_question.answer_options[0]}</li>
+            <li>{this.props.quiz_question.answer_options[0]}</li>
           </ul>
         </section>
-      </main>
-      )
-    }
+      </main>      
+    )
   }
-  
-  export default QuizQuestion
-  
+}
+
+export default QuizQuestion
