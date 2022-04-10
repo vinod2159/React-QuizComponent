ca5290ed2c982f51dfafd5e67037aed2d39d6310
vinod2159 <vinod.yadav@mindtree.com>
2022-04-10 14:53:57 +0530

    update for error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
-import Quiz from './Quiz'
+import Quiz from './Quiz.js'
 
 class App extends Component {
   render() {
--- src/Quiz.js
+++ src/Quiz.js
@@ -6,20 +6,17 @@ let quizData = require('./quiz_data.json')
 class Quiz extends Component {
     constructor(props) {
         super(props);
- 
-        this.state = {quiz_position: 1};
-
+         this.state = {quiz_position: 1};
       }
 
     render() {
       return  (
-        <div className='QuizQuestion'>
-              {quizData.quiz_questions[this.state.quiz_position].instruction_text};
+        <div className="QuizQuestion">
+              {quizData.quiz_questions[this.state.quiz_position].instruction_text}
           </div>
           )
     }
   }
-
 
   export default Quiz
 
